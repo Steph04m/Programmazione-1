5c54e72266d99bf9d84f2445e7af5a2631dde7ed
Stefano Mavilla <171691720+Steph04m@users.noreply.github.com>
2025-06-28 18:06:35 +0200

    Add files via upload

---
+++ Strutture_Dati/Coda.c
@@ -0,0 +1,122 @@
+//Implementazione di una Coda di interi in C
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Nodo{
+    int data;
+    struct Nodo* next;
+}Nodo;
+
+typedef struct Coda{
+    Nodo* head;
+    Nodo* tail;
+    int size;
+}Coda;
+
+Coda* getQueue()
+{
+    Coda* Queue = (Coda*)malloc(sizeof(Coda));
+    if(Queue == NULL){
+        fprintf(stderr, "Errore: Impossibile allocare la Coda.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    Queue->head = NULL;
+    Queue->tail = NULL;
+    Queue->size = 0;
+    return Queue;
+}
+
+bool isEmpty(Coda* Queue)
+{
+    if(Queue->size == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void EnQueue(Coda* Queue, int n)
+{
+    Nodo* newnode = (Nodo*)malloc(sizeof(Nodo));
+    if(newnode == NULL){
+        fprintf(stderr, "Errore: Impossibile allocare il nodo.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newnode->data = n;
+    newnode->next = NULL;
+    if(Queue->tail == NULL){
+        Queue->head = newnode;
+        Queue->tail = newnode;
+    } else {
+        Queue->tail->next = newnode;
+        Queue->tail = newnode;
+    }
+    Queue->size++;
+}
+
+void DeQueue(Coda* Queue)
+{
+    if(isEmpty(Queue)){
+        fprintf(stderr, "Errore: La Coda è vuota.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    Nodo* tmp = Queue->head;
+    Queue->head = Queue->head->next;
+    free(tmp);
+    Queue->size--;
+}
+
+void printQueue(Coda* Queue)
+{
+    Nodo* tmp = Queue->head;
+    while(tmp != NULL){
+        printf("%d ", tmp->data);
+        tmp = tmp->next;
+    }
+    printf("\n");
+}
+
+void clearQueue(Coda* Queue)
+{
+    while(Queue->head != NULL){
+        Nodo* tmp = Queue->head;
+        Queue->head = Queue->head->next;
+        free(tmp);
+        Queue->size--;
+    }
+    printf("Coda eliminata al termine del programma.\n");
+    free(Queue);
+}
+
+int main(int argc, char* argv[])
+{
+    Coda* Queue = getQueue();
+
+    EnQueue(Queue, 1);
+    EnQueue(Queue, 2);
+    EnQueue(Queue, 3);
+    EnQueue(Queue, 4);
+    EnQueue(Queue, 5);
+    EnQueue(Queue, 6);
+    EnQueue(Queue, 7);
+    EnQueue(Queue, 8);
+    EnQueue(Queue, 9);
+
+    printf("Contenuto della Coda:\n");
+    printQueue(Queue);
+    printf("Size Coda: %d\n", Queue->size);
+
+    DeQueue(Queue);
+    DeQueue(Queue);
+
+    printf("\n");
+    printf("Contenuto della Coda dopo due DeQueue:\n");
+    printQueue(Queue);
+    printf("Size Coda: %d\n", Queue->size);
+
+    clearQueue(Queue);
+}
---
+++ Strutture_Dati/Pila.c
@@ -0,0 +1,120 @@
+//Implementazione di una Pila di interi in C
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct Nodo{
+    int data;
+    struct Nodo* next;
+}Nodo;
+
+typedef struct Pila{
+    Nodo* head;
+    int size;
+}Pila;
+
+Pila* getStack()
+{
+    Pila* Stack = (Pila*)malloc(sizeof(Pila));
+    if(Stack == NULL){
+        fprintf(stderr, "Errore: Impossibile allocare la Pila.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    Stack->head = NULL;
+    Stack->size = 0;
+    return Stack;
+}
+
+bool isEmpty(Pila* Stack)
+{
+    if(Stack->size == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void Push(Pila* Stack, int n)
+{
+    Nodo* newnode = (Nodo*)malloc(sizeof(Nodo));
+    if(newnode == NULL){
+        fprintf(stderr, "Errore: Nodo non allocabile.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    newnode->data = n;
+    newnode->next = NULL;
+    
+    if(Stack->head == NULL){
+        Stack->head = newnode;
+    } else {
+        newnode->next = Stack->head;
+        Stack->head = newnode;
+    }
+    Stack->size++;
+}
+
+void Pop(Pila* Stack)
+{
+    if(isEmpty(Stack)){
+        fprintf(stderr, "La Pila è vuota.");
+        exit(EXIT_FAILURE);
+    }
+
+    Nodo* tmp = Stack->head;
+    Stack->head = Stack->head->next;
+    free(tmp);
+    Stack->size--;
+}
+
+void printStack(Pila* Stack)
+{
+    Nodo* tmp = Stack->head;
+    while(tmp != NULL){
+        printf("%d ", tmp->data);
+        tmp = tmp->next;
+    }
+    printf("\n");
+}
+
+void clearStack(Pila* Stack)
+{
+    while(Stack->head != NULL){
+        Nodo* tmp = Stack->head;
+        Stack->head = Stack->head->next;
+        free(tmp);
+        Stack->size--;
+    }
+    printf("Pila eliminata al termine del programma.\n");
+    free(Stack);
+}
+
+int main(int argc, char* argv[])
+{
+    Pila* Stack = getStack();
+    
+    Push(Stack, 1);
+    Push(Stack, 2);
+    Push(Stack, 3);
+    Push(Stack, 4);
+    Push(Stack, 5);
+    Push(Stack, 6);
+    Push(Stack, 7);
+    Push(Stack, 8);
+    Push(Stack, 9);
+
+    printf("Contenuto della Pila:\n");
+    printStack(Stack);
+    printf("Size Pila: %d\n", Stack->size);
+
+    Pop(Stack);
+    Pop(Stack);
+
+    printf("\n");
+    printf("Contenuto della Pila dopo due Pop:\n");
+    printStack(Stack);
+    printf("Size Pila: %d\n", Stack->size);
+
+    clearStack(Stack);
+}
